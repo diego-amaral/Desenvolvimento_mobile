f00932abbdffc30c9008b9ce76c1eed2418abe2d
Diego Amaral <diegoamaraldossantos1@gmail.com>
2021-06-03 14:29:43 -0300

    desenvolvimento da tela de login

--- Navigator/MainStack.js
+++ Navigator/MainStack.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import Home from '../Screens/Home'
+import Login from '../Screens/Login'
 import Segunda from '../Screens/Segunda'
 import Terceira from '../Screens/Terceira'
 
@@ -8,7 +8,7 @@ const MainStack = createStackNavigator()
 
 export default () => (
     <MainStack.Navigator>
-        <MainStack.Screen name="Home" component={Home} />
+        <MainStack.Screen name="Login" component={Login} />
         <MainStack.Screen name="Segunda" component={Segunda} />
         <MainStack.Screen name="Terceira" component={Terceira} />
     </MainStack.Navigator>
